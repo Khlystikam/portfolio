68eed0c226540827564b7cc95d169970d4384df8
Aleksandr Khlystik <aleksandr@dev-khlystikam.ru>
2025-02-10 09:40:20 +0300

    update 10.02.25_3

--- crypto-trading-bots/src/components/time-range/TimeRange.js
+++ crypto-trading-bots/src/components/time-range/TimeRange.js
@@ -3,22 +3,10 @@ import "./TimeRange.css";
 const TimeRange = ({ activeTime, setActiveTime }) => {
 
     const timeRangeName = [
-        { 
-            index: 1,
-            name: "24h",
-        },
-        { 
-            index: 2,
-            name: "7 days",
-        },
-        { 
-            index: 3,
-            name: "30 days",
-        },
-        { 
-            index: 4,
-            name: "All time",
-        },
+        {index: 1, name: "24h"},
+        {index: 2, name: "7 days"},
+        {index: 3, name: "30 days"},
+        {index: 4, name: "All time"},
     ];
 
     return (
